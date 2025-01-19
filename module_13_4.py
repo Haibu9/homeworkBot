b376cdee8014df348024ad42ac0b51007a1d1758
Haibu9 <hellobulat2@gmail.com>
2025-01-19 17:53:56 +0400

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,53 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+# не забыть
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+class UserState(StatesGroup):
+    age, growth, weight = State(), State(), State()
+
+@dp.message_handler(text = 'Calories')
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+@dp.message_handler(state= UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+@dp.message_handler(state= UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+'''для мужчин: 10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5;
+для женщин: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161.'''
+
+@dp.message_handler(state= UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight= message.text)
+    data = await state.get_data()
+    const = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age'])
+    await message.answer(f'Ваша норма каллорий если вы мужчина: {const + 5} если вы женщина: {const - 161}')
+    await state.finish()
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_massages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+    # await message.answer(f'ваше сообщение: {message.text}')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
